18b660d356955761d14280451b42b03ba383b584
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-02-11 16:19:25 +0100

    Add sources

---
+++ Basics.thy
@@ -0,0 +1,46 @@
+(*  Title:      Separation-Logic-Formalization/Basics.thy
+    Author:     Nicolas Amat
+*)
+
+section {* Formalization *}
+theory Basics
+  imports Main HOL.Map
+begin
+
+text {* Definition of the Syntax and Semantics *}
+
+subsection {* Some Types Definitions *}
+
+(* Stores, Heaps, States *)
+type_synonym var = string
+type_synonym addr = nat
+type_synonym val = int
+type_synonym store = "var \<Rightarrow> val"
+type_synonym heap = "addr \<rightharpoonup> val"
+type_synonym state = "(store \<times> heap)"
+
+type_synonym exp = "store \<Rightarrow> val"
+type_synonym bexp = "store \<Rightarrow> bool"
+
+datatype 'a sl_formula =
+    True
+  | False
+  | emp
+  | sl_not "'a sl_formula"
+  | sl_impl "'a sl_formula" "'a sl_formula"
+  | sl_conj "'a sl_formula" "'a sl_formula"
+  | sl_magic_wand "'a sl_formula" "'a sl_formula"
+
+
+subsection {* Some Functions Definitions *}
+
+abbreviation empty_heap :: heap where
+  "empty_heap \<equiv> Map.empty"
+
+definition disjoint_heaps  where
+  "disjoint_heaps h1 h2 \<longleftrightarrow> (dom h1) \<inter> (dom h2) = {}"
+
+definition union_heaps where
+  "union_heaps h1 h2 = h1 ++ h2"
+
+end
