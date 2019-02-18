f4cb832d10a2fcb527874ccfa773361efca90f89
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-02-18 13:57:34 +0100

    Interp, store, heap and beginning of the semantic

--- Basics.thy
+++ Basics.thy
@@ -24,8 +24,16 @@ type_synonym state = "(store \<times> heap)"
 definition a_heap  where
   "a_heap h \<longleftrightarrow> finite (dom h)"
 
-typedef ('a, 'b) heaps = "{(h::'a \<Rightarrow> 'b option). a_heap h}" unfolding a_heap_def
-  using finite_dom_map_of by auto
+typedef ('a, 'b) heaps = "{(h::'a \<Rightarrow> 'b option). a_heap h}"
+  unfolding a_heap_def using finite_dom_map_of by auto
+
+type_synonym ('a, 'b, 'c) interp = "('a \<Rightarrow> 'b) \<times> (('b, 'c) heaps)"
+
+definition store::"('a, 'b, 'c) interp => 'a \<Rightarrow> 'b" 
+  where "store I = fst I"
+
+definition heap::"('a, 'b, 'c) interp \<Rightarrow> ('b, 'c) heaps"
+  where "heap I = snd I"
 
 (* Formula Syntax *)
 datatype 'a sl_formula =
@@ -33,11 +41,11 @@ datatype 'a sl_formula =
     true
   | false
   (* Classical Logic *)
+  | eq 'a 'a
   | not "'a sl_formula"
   | impl "'a sl_formula" "'a sl_formula"
   | conj "'a sl_formula" "'a sl_formula"
   | disj "'a sl_formula" "'a sl_formula"
-  | eq 'a 'a
   (* Quantifier *)
   | forall 'a "'a sl_formula"
   | exists 'a "'a sl_formula"
@@ -46,6 +54,18 @@ datatype 'a sl_formula =
   | sl_mapsto 'a 'a
   | sl_conj "'a sl_formula" "'a sl_formula"
   | sl_magic_wand "'a sl_formula" "'a sl_formula"
+
+
+fun evaluation::"(('a, 'b, 'c) interp) \<Rightarrow> 'd sl_formula \<Rightarrow> bool"
+where 
+    "evaluation I true         = True"
+  | "evaluation I false        = False" 
+  | "evaluation I (eq x y)     \<longleftrightarrow> (x = y)"
+  | "evaluation I (not f)      \<longleftrightarrow> \<not>(evaluation I f)"
+  | "evaluation I (impl f g)   \<longleftrightarrow> ((evaluation I f) \<longrightarrow> (evaluation I g))"
+  | "evaluation I (conj f g)   \<longleftrightarrow> ((evaluation I f) \<and> (evaluation I g))"
+  | "evaluation I (disj f g)   \<longleftrightarrow> ((evaluation I f) \<or> (evaluation I g))"
+
 
 subsection {* Some Functions Definitions *}
 
