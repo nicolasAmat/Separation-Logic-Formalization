76b873f60435d0b262a975fb9f167ec7d719e35f
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-01 16:04:58 +0100

    Interpretation theory corrections

--- Interpretation.thy
+++ Interpretation.thy
@@ -7,7 +7,10 @@ section {* Interpretations *}
 text {* This section contains the formalization of an interpretations in the separation logic. *}
 
 theory Interpretation
-  imports Main HOL.Map
+  imports 
+    Main 
+    "HOL.Map" 
+    "HOL-Analysis.Finite_Cartesian_Product"
 begin
 
 
@@ -16,57 +19,60 @@ subsection {* Heap *}
 definition a_heap  where
   "a_heap h \<longleftrightarrow> finite (dom h)"
 
-(* NP:  En principe une heap est une fonction de 'a vers un vecteur (liste) de 'a) pas une fonction   
-        vers un type arbitraire 'b *)
-
-typedef ('a, 'b) heaps = "{(h::'a \<Rightarrow> 'b option). a_heap h}" unfolding a_heap_def
+typedef ('addr, 'k) heaps = "{(h::'addr \<Rightarrow> (('addr, 'k) vec) option). a_heap h}" unfolding a_heap_def
   using finite_dom_map_of by auto
+
 
 subsection {* Interpretation *}
 
-(* NP:  avec ce qui est dit ci-dessus, il ne devrait pas y avoir 3 types mais 2 types: le type 
-        des variables et celui des adresses
-        Je suggère d'utiiser des notations consistantes pour dénoter ces types, eg 'a le type
-        des variables et 'b le type des adresses, et respecter ces conventions partout. 
-        Ou mieux encore ecrire explicitement 'var et 'addr *)
+type_synonym ('var, 'addr, 'k) interp = "('var \<Rightarrow> 'addr) \<times> (('addr, 'k) heaps)"
 
-type_synonym ('a, 'b, 'c) interp = "('a \<Rightarrow> 'b) \<times> (('b, 'c) heaps)"
 
 subsection {* Heap and Store from an Interpretation *}
 
-definition store :: "('a, 'b, 'c) interp => 'a \<Rightarrow> 'b" 
+definition store :: "('var, 'addr, 'k) interp \<Rightarrow> ('var \<Rightarrow> 'addr)" 
   where "store I = fst I"
 
-definition heap :: "('a, 'b, 'c) interp \<Rightarrow> ('b, 'c) heaps"
+definition heap :: "('var, 'addr, 'k) interp \<Rightarrow> ('addr, 'k) heaps"
   where "heap I = snd I"
+
+
+subsection {* Interpretation from an Heap and a Store *}
+
+definition constructor_interp :: "('var \<Rightarrow> 'addr) \<Rightarrow> (('addr, 'k) heaps) \<Rightarrow> ('var, 'addr, 'k) interp"
+  where "constructor_interp s h = (s, h)"
+
 
 subsection {* Heaps Operations *}
 
-definition dom::"('a, 'b) heaps \<Rightarrow> 'a set" where
+definition dom::"('addr, 'k) heaps \<Rightarrow> 'addr set" where
   "dom h = {a. Rep_heaps h a \<noteq> None}"
 
-definition disjoint_heaps :: "('a, 'b) heaps \<Rightarrow> ('a, 'b) heaps \<Rightarrow> bool" where
+definition disjoint_heaps :: "('addr, 'k) heaps \<Rightarrow> ('addr, 'k) heaps \<Rightarrow> bool" where
   "disjoint_heaps h1 h2 \<longleftrightarrow> (dom h1) \<inter> (dom h2) = {}"
 
-definition union_heaps :: "('a, 'b) heaps \<Rightarrow> ('a, 'b) heaps \<Rightarrow> ('a, 'b) heaps" where
-  "union_heaps h1 h2 = Abs_heaps ((Rep_heaps h1)++(Rep_heaps h2))"
+definition union_heaps :: "('addr, 'k) heaps \<Rightarrow> ('addr, 'k) heaps \<Rightarrow> ('addr, 'k) heaps" where
+  "union_heaps h1 h2 = Abs_heaps ((Rep_heaps h1) ++ (Rep_heaps h2))"
 
-definition equal_heaps :: "('a, 'b) heaps \<Rightarrow> ('a, 'b) heaps \<Rightarrow> bool" where
+definition equal_heaps :: "('addr, 'k) heaps \<Rightarrow> ('addr, 'k) heaps \<Rightarrow> bool" where
   "equal_heaps h1 h2 = ((Rep_heaps h1 \<subseteq>\<^sub>m Rep_heaps h2) \<and> (Rep_heaps h2 \<subseteq>\<^sub>m Rep_heaps h1))"
 
+definition empty_heaps :: "('addr, 'k) heaps \<Rightarrow> bool" where
+  "empty_heaps h \<equiv> Rep_heaps h = Map.empty"
+  
 
 subsection {* Useful Heaps Results *}
 
 lemma finite_union_heaps:
-  assumes "finite (Map.dom (h1::('a \<Rightarrow> 'b option)))"
+  assumes "finite (Map.dom (h1::'addr \<Rightarrow> (('addr,'k) vec) option))"
     and "finite (Map.dom h2)"
-  shows "finite (Map.dom (h1++h2))"
+  shows "finite (Map.dom (h1 ++ h2))"
 proof -
   have finite_union_domains: "finite ((Map.dom h1) \<union> (Map.dom h2))"
     by (simp add: assms(1) assms(2))
   have union_inlude_into_union_domains:"Map.dom(h1 ++ h2) \<subseteq> (Map.dom h1) \<union> (Map.dom h2)"
     by simp
-  from finite_union_domains and union_inlude_into_union_domains show "finite (Map.dom(h1++h2))"
+  from finite_union_domains and union_inlude_into_union_domains show "finite (Map.dom(h1 ++ h2))"
     by auto
 qed
 
