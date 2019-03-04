f634b3aefde887d6f43f7d69c700526ee559e18f
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-04 16:07:52 +0100

    Explicit variables and mapsto

--- Formula.thy
+++ Formula.thy
@@ -15,31 +15,27 @@ begin
 
 subsection {* Formulas Syntax *}
 
-datatype ('a, 'b) sl_formula =
+datatype ('var, 'k) sl_formula =
   (* Boolean *)
     true
   | false
   (* Classical Logic *)
-  | eq 'a 'a
-  | not "('a, 'b) sl_formula"
-  | conj "('a, 'b) sl_formula" "('a, 'b) sl_formula"
-  | disj "('a, 'b) sl_formula" "('a, 'b) sl_formula"
+  | eq 'var 'var
+  | not "('var, 'k) sl_formula"
+  | conj "('var, 'k) sl_formula" "('var, 'k) sl_formula"
+  | disj "('var, 'k) sl_formula" "('var, 'k) sl_formula"
   (* Quantifier *)
-  | exists 'a "('a, 'b) sl_formula"
+  | exists 'var "('var, 'k) sl_formula"
   (* Separation Logic *)
   | sl_emp
-  | sl_mapsto 'a "('a, 'b) vec"
-  | sl_conj "('a, 'b) sl_formula" "('a, 'b) sl_formula"
-  | sl_magic_wand "('a, 'b) sl_formula" "('a, 'b) sl_formula"
+  | sl_mapsto 'var "('var, 'k) vec"
+  | sl_conj "('var, 'k) sl_formula" "('var, 'k) sl_formula"
+  | sl_magic_wand "('var, 'k) sl_formula" "('var, 'k) sl_formula"
 
 
 subsection {* Formulas Semantics *}
 
-(* NP: la définition de la semantique de sl_emp et sl_mapsto est incorrecte *)
-(* pour sl_emp il faut vérifier que le domaine de la heap est vide et pour sl_mapsto il faut
-   vérifier que le domaine est réduit à E et que heap I E = F *)
-
-primrec evaluation :: "(('a, 'b, 'c) interp) \<Rightarrow> ('a, 'c) sl_formula \<Rightarrow> bool"
+primrec evaluation :: "(('var, 'addr, 'k) interp) \<Rightarrow> ('var, 'k) sl_formula \<Rightarrow> bool"
   where 
     "evaluation I true                = True"
   | "evaluation I false               = False" 
@@ -47,9 +43,10 @@ primrec evaluation :: "(('a, 'b, 'c) interp) \<Rightarrow> ('a, 'c) sl_formula \
   | "evaluation I (not P)             =  (\<not>(evaluation I P))"
   | "evaluation I (conj P Q)          = ((evaluation I P) \<and> (evaluation I Q))"
   | "evaluation I (disj P Q)          = ((evaluation I P) \<or> (evaluation I Q))"
-  | "evaluation I (exists x P)        = (\<exists>u. (evaluation ((store I)(x:=u),(heap I)) P))"
+  | "evaluation I (exists x P)        = (\<exists>u. (evaluation (constructor_interp ((store I)(x:=u)) (heap I)) P))" 
   | "evaluation I (sl_emp)            = empty_heaps (heap I)"
-  | "evaluation I (sl_mapsto x y)     = True"
+  | "evaluation I (sl_mapsto x y)     = ((dom (heap I) = {(store I) x})) "
+                                     (*  \<and> ( (Rep_heaps (heap I)) ((store I) x) = (store I) y))" *)
   | "evaluation I (sl_conj P Q)       = (\<exists>h1 h2. (union_heaps h1 h2 = heap I)
                                                \<and> (disjoint_heaps h1 h2) 
                                                \<and> (evaluation (constructor_interp (store I) h1) P) 
