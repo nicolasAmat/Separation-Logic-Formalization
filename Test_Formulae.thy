5cb8663b729d66903be4277333799162e6875a1b
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-04 15:08:58 +0100

    Add Test_Formulae file

---
+++ Test_Formulae.thy
@@ -0,0 +1,23 @@
+(*  Title:      Separation-Logic-Formalization/Test_Formulae.thy
+    Author:     Nicolas Amat, Mnacho Echenim, Nicolas Peltier
+*)
+
+section {* Test Formulae *}
+
+text {* This section contains test formulae. *}
+
+theory Test_Formulae
+  imports 
+    Main 
+    Interpretation
+    Formula
+begin
+
+
+subsection {* Points to *}
+
+
+subsection {* Alloc *}
+
+
+end
