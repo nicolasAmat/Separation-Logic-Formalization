128c91c221fe453052948f7084068f30df8efda6
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-04 15:47:32 +0100

    Add the Modus Ponens file

---
+++ Modus_Ponens.thy
@@ -0,0 +1,27 @@
+(*  Title:      Separation-Logic-Formalization/Modus_Ponens.thy
+    Author:     Nicolas Amat, Mnacho Echenim, Nicolas Peltier
+*)
+
+section {* Modus Ponens *}
+
+text {* This section contains Modus Ponens like for the sepration logic. *}
+
+theory Modus_Ponens
+  imports 
+    Main 
+    Interpretation
+    Formula
+begin
+
+
+subsection {* Proof *}
+
+lemma modus_ponens:
+  fixes I::"('var, 'addr, 'k) interp" and P::"('var, 'k) sl_formula" and Q::"('var, 'k) sl_formula"
+  assumes  "evaluation I (sl_conj P (sl_magic_wand P Q))"
+  shows "evaluation I Q"
+proof -
+  oops
+
+
+end
