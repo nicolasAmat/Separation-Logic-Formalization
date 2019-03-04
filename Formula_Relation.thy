125eb8e3801028c0daf99b6add2147f976bf9c8b
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-04 15:08:13 +0100

    Logical consequence and equivalence

---
+++ Formula_Relation.thy
@@ -0,0 +1,29 @@
+(*  Title:      Separation-Logic-Formalization/Formula_Relation.thy
+    Author:     Nicolas Amat, Mnacho Echenim, Nicolas Peltier
+*)
+
+section {* Relations *}
+
+text {* This section contains the logical consequence and the equivalence. *}
+
+theory Formula_Relation
+  imports 
+    Main 
+    Interpretation
+    Formula
+begin
+
+
+subsection {* Logical Consequence *}
+
+definition logical_consequence :: "'addr \<Rightarrow> ('var, 'k) sl_formula \<Rightarrow> ('var, 'k) sl_formula \<Rightarrow> bool" where
+  "logical_consequence addr f g = (\<forall>I::('var, 'addr, 'k) interp. (evaluation I f) \<longrightarrow> (evaluation I g))"
+
+
+subsection {* Equivalence *}
+
+definition equivalence :: "'addr \<Rightarrow> ('var, 'k) sl_formula \<Rightarrow> ('var, 'k) sl_formula \<Rightarrow> bool" where
+  "equivalence addr f g = ((logical_consequence addr f g) \<and> (logical_consequence addr g f))"
+
+
+end
