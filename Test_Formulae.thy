0555fb9384c4c0c2ac04a98d4ad4ff9fc90c14e1
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-05 22:56:06 +0100

    Test_Formulae.thy beginning

--- Test_Formulae.thy
+++ Test_Formulae.thy
@@ -7,17 +7,26 @@ section {* Test Formulae *}
 text {* This section contains test formulae. *}
 
 theory Test_Formulae
-  imports 
-    Main 
-    Interpretation
+  imports
     Formula
 begin
 
 
-subsection {* Points to *}
+subsection {* Syntax *}
+
+datatype ('var, 'k::finite) test_formulae = 
+  (* Points to *)
+    points_to 'var "('var, 'k::finite) vec"
+  (* Alloc *)
+  | alloc 'var
 
 
-subsection {* Alloc *}
+subsection {* Definitions *}
+
+primrec tf_definition :: "('var, 'k::finite) test_formulae \<Rightarrow> ('var, 'k::finite) sl_formula"
+  where
+    "tf_definition (points_to x y) = sl_conj (sl_mapsto x y) true"
+  | "tf_definition (alloc x)       = sl_magic_wand (sl_mapsto x (vec x)) false"
 
 
 end
