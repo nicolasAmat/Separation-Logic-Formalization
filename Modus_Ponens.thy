e7ec38ee9cb046514b519c71b05db5269a44dd7b
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-05 10:47:23 +0100

    Proof: Modus Ponens

--- Modus_Ponens.thy
+++ Modus_Ponens.thy
@@ -23,6 +23,19 @@ lemma modus_ponens:
   assumes  "evaluation I (sl_conj P (sl_magic_wand P Q))"
   shows "evaluation I Q"
 proof -
-  oops
+  have def_1:"\<exists>h1::('addr, 'k) heaps. 
+        \<exists>h2::('addr, 'k) heaps. (union_heaps h1 h2 = heap I)
+                              \<and> (disjoint_heaps h1 h2)
+                              \<and> (evaluation (constructor_interp (store I) h1) P) 
+                              \<and> (evaluation (constructor_interp (store I) h2) (sl_magic_wand P Q))"
+    using assms evaluation.simps(10) by blast
+  then have def_2: "\<exists>h2::('addr, 'k) heaps. 
+                  \<forall>h3::('addr, 'k) heaps. ((disjoint_heaps h3 h2) \<and> (evaluation (constructor_interp (store I) h3) P))
+                                          \<longrightarrow> (evaluation (constructor_interp (store I) (union_heaps h2  h3)) Q)"
+    by (metis constructor_interp_def evaluation.simps(11) fst_conv store_def)
+  from def_1 and def_2 show "evaluation I Q"
+    by (metis commutative_union_disjoint_heaps constructor_interp_def eq_fst_iff evaluation.simps(11) heap_def snd_conv store_def)
+qed
+
 
 end
