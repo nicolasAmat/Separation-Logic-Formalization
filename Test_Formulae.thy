f5ed6e6b1ec1e374c66b54e37e8435f0e39d62e9
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-06 15:58:15 +0100

    Add size_heaps_superior_to syntax

--- Test_Formulae.thy
+++ Test_Formulae.thy
@@ -9,25 +9,28 @@ text {* This section contains test formulae. *}
 theory Test_Formulae
   imports
     Formula
+    "HOL-Library.Extended_Nat"
 begin
 
 
 subsection {* Syntax *}
 
-datatype ('var, 'k::finite) test_formulae = 
-  (* Points to *)
+datatype ('var, 'addr, 'k::finite) test_formulae = 
+  (* Points-to Relations in the Heap *)
     points_to 'var "('var, 'k::finite) vec"
-  (* Alloc *)
+  (* Allocation *)
   | alloc 'var
-(* TODO : |h| \<ge> n *)
+  (* Cardinality Constraint *)
+  | size_heaps_superior_to "('addr, 'k) heaps" enat 
+
 
 subsection {* Definitions *}
 
-primrec tf_definition :: "('var, 'k::finite) test_formulae \<Rightarrow> ('var, 'k::finite) sl_formula"
+primrec tf_definition :: "('var, 'addr, 'k::finite) test_formulae \<Rightarrow> ('var, 'k::finite) sl_formula"
   where
     "tf_definition (points_to x y) = sl_conj (sl_mapsto x y) true"
   | "tf_definition (alloc x)       = sl_magic_wand (sl_mapsto x (vec x)) false"
-(* TODO : |h| \<ge> n *)
+(* TODO *)
 
 
 end
