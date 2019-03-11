c549589196dfbd239d69b49fced241c541cff23e
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-11 11:20:52 +0100

    Current work

--- Test_Formulae.thy
+++ Test_Formulae.thy
@@ -32,10 +32,34 @@ primrec card_heaps_superior_to :: "('addr, 'k) heaps \<Rightarrow> nat \<Rightar
       "card_heaps_superior_to h (Suc n) = sl_conj (card_heaps_superior_to h n) (not sl_emp)"
     | "card_heaps_superior_to h 0 = true"
 
-primrec extended_card_heaps_superior_to ::  "('addr, 'k) heaps \<Rightarrow> enat \<Rightarrow> ('var, 'k::finite) sl_formula"
+primrec ext_card_heaps_superior_to ::  "('addr, 'k) heaps \<Rightarrow> enat \<Rightarrow> ('var, 'k::finite) sl_formula"
   where
-      "extended_card_heaps_superior_to h \<infinity> = false"
-    | "extended_card_heaps_superior_to h n = card_heaps_superior_to h n"
+      "ext_card_heaps_superior_to h \<infinity> = false"
+    | "ext_card_heaps_superior_to h n = card_heaps_superior_to h n"
+
+(*
+function other_way :: "('addr, 'k) heaps \<Rightarrow> enat \<Rightarrow> ('var, 'k::finite) sl_formula"
+  where
+       "other_way h (enat (Suc n)) = other_way h n"
+     | "other_way h (enat 0) = true"
+     | "other_way h \<infinity> = false"
+  by pat_completeness auto
+termination 
+  proof -
+    let ?R = "{((h, n::enat), (h, m::enat)). less_enat n m}"
+    show ?thesis
+    proof(relation ?R)
+      show "wf ?R"
+*)
+
+
+subsection {* Inductive Set *}
+
+(*
+inductive_set test_forumulae :: "('var, 'k::finite) sl_formula set"
+  where
+  ""
+*)
 
 
 end
