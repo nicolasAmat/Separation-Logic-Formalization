bd7689a1294f22b91510490e69855ed751ab00e3
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-11 20:52:07 +0100

    Revert Modus_Ponens.thy

--- Modus_Ponens.thy
+++ Modus_Ponens.thy
@@ -22,18 +22,18 @@ lemma modus_ponens:
   assumes  "evaluation I (sl_conj P (sl_magic_wand P Q))"
   shows "evaluation I Q"
 proof -
-  have "\<exists>h1::('addr, 'k) heaps. 
-              \<exists>h2::('addr, 'k) heaps. (union_heaps h1 h2 = heap I)
-                                    \<and> (disjoint_heaps h1 h2)
-                                    \<and> (evaluation (to_interp (store I) h1) P) 
-                                    \<and> (evaluation (to_interp (store I) h2) (sl_magic_wand P Q))"
-    using assms by simp
-  moreover hence "\<exists>h2::('addr, 'k) heaps. 
+  have def_1: "\<exists>h1::('addr, 'k) heaps. 
+               \<exists>h2::('addr, 'k) heaps. (union_heaps h1 h2 = heap I)
+                                     \<and> (disjoint_heaps h1 h2)
+                                     \<and> (evaluation (to_interp (store I) h1) P) 
+                                     \<and> (evaluation (to_interp (store I) h2) (sl_magic_wand P Q))"
+    using assms evaluation.simps(9) by blast 
+  hence def_2: "\<exists>h2::('addr, 'k) heaps. 
                     \<forall>h3::('addr, 'k) heaps. ((disjoint_heaps h3 h2) \<and> (evaluation (to_interp (store I) h3) P))
                                          \<longrightarrow> (evaluation (to_interp (store I) (union_heaps h2  h3)) Q)"
-    by (metis to_interp_def evaluation.simps(10) fst_conv store_def)
-  ultimately show "evaluation I Q"
-    by (metis commutative_union_disjoint_heaps to_interp_def eq_fst_iff evaluation.simps(10) heap_def snd_conv store_def)
+    by (metis evaluation.simps(10) fst_conv store_def to_interp_def)
+  from def_1 and def_2  show "evaluation I Q"
+    by (metis commutative_union_disjoint_heaps evaluation.simps(10) fst_conv heap_def prod.exhaust_sel snd_conv store_def to_interp_def)  
 qed
 
 
