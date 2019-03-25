162816f4dfc3748418622dea5bea65f2b7f1e12f
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-03-25 17:10:10 +0100

    Add Minterm.thy file

---
+++ Minterm.thy
@@ -0,0 +1,16 @@
+(*  Title:      Separation-Logic-Formalization/Minterm.thy
+    Author:     Nicolas Amat, Mnacho Echenim, Nicolas Peltier
+*)
+
+section {* Minterms *}
+
+text {* This section contains the minterms definition and some propositions related to them. *}
+
+theory Minterm
+imports
+  Test_Formulae
+begin
+
+
+subsection {* Definition *}
+
